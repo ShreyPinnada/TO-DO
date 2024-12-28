7512e9411d0d5d1f229e328ba6ef9ebac7394492
ShreyPinnada <shrey30000official@gmail.com>
2024-12-28 17:03:04 +0530

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,46 @@ function createNewTask() {
   //handle this later
   if (taskTitle == "") {
     window.alert("Please enter the Title!");
-  } else {
-    addTaskToRightPanel(taskTitle, taskDescription);
-    totalTasks++;
-    updateCounts();
+    return;
   }
+
+  const task = {
+    task: taskTitle,
+    description: taskDescription,
+    completed: false,
+  };
+
+  fetch("http://localhost:8080/todo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  })
+    .then((respose) => respose.json())
+    .then((data) => {
+      addTaskToRightPanel(data.task, data.description, data.id, data.completed);
+      totalTasks++;
+      updateCounts();
+    })
+    .catch((error) => console.error("Error while creating "));
   document.getElementById("title-textbox").value = "";
   document.getElementById("description-textbox").value = "";
 }
 
-function addTaskToRightPanel(taskTitle, taskDescription) {
+function addTaskToRightPanel(taskTitle, taskDescription, id, completed) {
   const taskDiv = document.createElement("div");
   taskDiv.classList.add("task");
+  taskDiv.dataset.id = id;
 
   const taskTitleElement = document.createElement("span");
   taskTitleElement.classList.add("task-title");
   taskTitleElement.textContent = taskTitle;
+
+  if (completed) {
+    taskTitleElement.innerHTML = `<strike>${taskTitle}</strike>`;
+    completedTasks++;
+  }
 
   const taskButtonsDiv = document.createElement("div");
   taskButtonsDiv.classList.add("task-buttons");
@@ -60,25 +84,43 @@ function addTaskToRightPanel(taskTitle, taskDescription) {
   });
 
   completedBtn.addEventListener("click", () => {
-    if (!taskTitleElement.innerHTML.includes("<strike>")) {
-      taskTitleElement.innerHTML = `<strike>${taskTitle}</strike>`;
-      completedTasks++;
-    } else {
-      taskTitleElement.innerHTML = taskTitle;
-      completedTasks--;
-    }
-    updateCounts();
+    const isCompleted = taskTitleElement.innerHTML.includes("<strike>");
+    fetch(`http://localhost:8080/todo/${id}?completed=${!isCompleted}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed: !isCompleted }),
+    })
+      .then((response) => response.json())
+      .then((updatedTask) => {
+        if (updatedTask.completed) {
+          taskTitleElement.innerHTML = `<strike>${taskTitle}</strike>`;
+          completedTasks++;
+        } else {
+          taskTitleElement.innerHTML = taskTitle;
+          completedTasks--;
+        }
+        updateCounts();
+      })
+      .catch((error) => console.error("Error:", error));
   });
 
   deleteBtn.addEventListener("click", () => {
     const confirmDelete = window.confirm("Are you sure you want to Delete ?");
     if (confirmDelete) {
-      taskDiv.remove();
-      totalTasks--;
-      if (taskTitleElement.innerHTML.includes("<strike>")) {
-        completedTasks--;
-      }
-      updateCounts();
+      fetch(`http://localhost:8080/todo/${id}`, {
+        method: "DELETE",
+      })
+        .then(() => {
+          taskDiv.remove();
+          totalTasks--;
+          if (taskTitleElement.innerHTML.includes("<strike>")) {
+            completedTasks--;
+          }
+          updateCounts();
+        })
+        .catch((error) => console.error("Error:", error));
     }
   });
 
@@ -101,10 +143,10 @@ function updateCounts() {
 }
 
 function createModalTitleAndDescription(title, description) {
-  const existingModal = document.querySelector(".modal-overlay");
-  if (existingModal) {
-    existingModal.remove();
-  }
+  // const existingModal = document.querySelector(".modal-overlay");
+  // if (existingModal) {
+  //   existingModal.remove();
+  // }
   const modalOverlay = document.createElement("div");
   modalOverlay.classList.add("modal-overlay");
 
@@ -130,3 +172,25 @@ function createModalTitleAndDescription(title, description) {
   modalOverlay.appendChild(modalContent);
   document.body.appendChild(modalOverlay);
 }
+
+function loadTasks() {
+  fetch("http://localhost:8080/todo")
+    .then((response) => response.json())
+    .then((tasks) => {
+      tasks.forEach((task) => {
+        addTaskToRightPanel(
+          task.task,
+          task.description,
+          task.id,
+          task.completed
+        );
+        totalTasks++;
+        // if (task.completed) {
+        //   completedTasks++;
+        // }
+      });
+      updateCounts();
+    })
+    .catch((error) => console.error("Error:", error));
+}
+document.addEventListener("DOMContentLoaded", loadTasks);
